b7730207024a9215bd35e43c8f259393571e661a
Chandan Kumar <119083293+its-chandan16@users.noreply.github.com>
2024-07-22 02:59:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+var sidemeu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemeu.style.right = "0";
+}
+function closemenu(){
+    sidemeu.style.right = "-200px";
+}
+document.getElementsByClassName("btn").addEventListener("click", function(event) 
+{
+    event.preventDefault(); // Prevents the form from submitting
+    alert("Button clicked!");
+});
